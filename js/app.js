274eaa4f4bb84f777454d618208e0a32df189e42
thatoneguyadf <adamfield@live.com>
2015-07-06 14:21:37 -0700

    updated js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ person.name = prompt("Please enter your name.");
 person.email = prompt("Please enter your email address.");
 person.favoriteMovie = prompt("Please enter your favorite movie.");
 
-console.log(person.name);
-console.log(person.email);
-console.log(person.favoriteMovie);
+
+console.log(person);
+//console.log(person.name);
+//console.log(person.email);
+//console.log(person.favoriteMovie);
