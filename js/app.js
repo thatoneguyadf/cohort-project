e7692b7bfd67d0d757222d7c97be3d0ad8e081d8
thatoneguyadf <adamfield@live.com>
2015-07-06 14:16:49 -0700

    adding html and js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var person = {};
+
+person.name = prompt("Please enter your name.");
+person.email = prompt("Please enter your email address.");
+person.favoriteMovie = prompt("Please enter your favorite movie.");
+
+console.log(person.name);
+console.log(person.email);
+console.log(person.favoriteMovie);
