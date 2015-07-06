bd10ade17a13f8dfe594a0fe4e32130370f78f10
thatoneguyadf <adamfield@live.com>
2015-07-06 16:25:18 -0700

    refactored folder structure to allow multiple projects and addded number project

---
+++ numbers/js/app.js
@@ -0,0 +1,22 @@
+function math() {
+	var numOne = prompt("Please enter a number.");
+	var numTwo = prompt("Please enter a second number.");
+
+	var parseNumOne = parseInt(numOne);
+	var parseNumTwo = parseInt(numTwo);
+
+	if(isNaN(parseNumOne) || isNaN(parseNumTwo)) {
+		alert("You must enter numbers!");
+		var reset = confirm("Woud you like to try again?");
+		if(reset) {
+			math();
+		}
+		else {
+			alert("Good bye.");
+		}
+	}
+	else {
+		var addition = parseNumOne + parseNumTwo;
+		console.log(addition);
+	}
+}
