8f3585bebc813bf60bd3858bc9c1fdd03b3f2629
thatoneguyadf <adamfield@live.com>
2015-07-07 16:05:45 -0700

    updated js for topTenMovies project

--- topTenMovies/js/apps.js
+++ topTenMovies/js/apps.js
@@ -25,12 +25,11 @@ for(var i = 0; i < movies.length; i++) {
 	var titleWords = movies[i].split(' ');
 	for(var j = 0; j < titleWords.length; j++) {
 		var noCap = ['of', 'the', 'and', 'for', 'in', 'to']
-		if(j != 0 && noCap.indexOf(titleWords[j]) > -1) {
+		if(j !== 0 && noCap.indexOf(titleWords[j]) > -1) {
 			continue;
 		}
-		else {
-			titleWords[j] = capitalize(titleWords[j]);
-		}
+
+		titleWords[j] = capitalize(titleWords[j]);
 	}
 
 	titleWords = titleWords.join(' ');
