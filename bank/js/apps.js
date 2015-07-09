f70465b7b40c1673d84df61340ecf47332c84969
thatoneguyadf <adamfield@live.com>
2015-07-09 00:05:38 -0700

    finished bank project

--- bank/js/apps.js
+++ bank/js/apps.js
@@ -3,11 +3,16 @@
 /*global confirm*/
 /*global console*/
 
+
+//retireves DOM elements to input data in HTML
+var sBalance = document.getElementById('sBalance');
+var transact = document.getElementById('transact');
+var eBalance = document.getElementById('eBalance');
+
 //creates Bank objects
 function Bank() {
     this.balance = 100;
 }
-
 
 //adds prototypes to Bank objects
 Bank.prototype = {
@@ -31,18 +36,31 @@ Bank.prototype = {
 
 var bank = new Bank();
 
+sBalance.textContent = '$' + bank.balance;
+
+//loops three times and does credits
 for(var i = 0; i < 3; i++) {
     var credInput = prompt('Enter an amount to be credited to your account.');
+    var credLi = document.createElement('li');
     credInput = parseInt(credInput);
     bank.credit(credInput);
+    credLi.textContent = '$' + credInput + ' credit. Current balance: $' + bank.balance;
+    transact.appendChild(credLi);
 }
 
 console.log('Bank balance after three credits is: ' + bank.balance);
 
+
+//loops twice and does debits
 for(var j = 0; j < 2; j++) {
     var debInput = prompt('Enter an amount to be credited to your account.');
+    var debLi = document.createElement('li');
     debInput = parseInt(debInput);
     bank.debit(debInput);
+    debLi.textContent = '$' + debInput + ' debit. Current balance: $' + bank.balance;
+    transact.appendChild(debLi);
 }
 
 console.log('Final bank balance after two debits is: ' + bank.balance);
+
+eBalance.textContent = '$' + bank.balance;
