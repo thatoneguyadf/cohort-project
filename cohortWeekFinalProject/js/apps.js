6d96ab6d5b5ac20be3095b2072f25193673a3652
thatoneguyadf <adamfield@live.com>
2016-06-13 11:09:46 -0700

    implemented local storage

--- cohortWeekFinalProject/js/apps.js
+++ cohortWeekFinalProject/js/apps.js
@@ -1,11 +1,11 @@
-/*============================== variable space ========================================*/
+/*============================================ variables ============================================================*/
 
 var list = document.getElementById('movies');
 var search = document.getElementById('search');
 var newMovieBtn = document.querySelector('.newMovie');
 var newMovieForm = document.getElementById('movieFormContainer');
 var movieForm = document.getElementById('movieForm');
-var movies = [
+var movies = localStorage.movies ? JSON.parse(localStorage.movies) : [
     ['star wars', 121, 1977, 'drama', 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empire\'s world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.'],
     ['empire strikes back', 124, 1980, 'drama', 'After the rebels have been brutally overpowered by the Empire on their newly established base, Luke Skywalker takes advanced Jedi training with Master Yoda, while his friends are pursued by Darth Vader as part of his plan to capture Luke.'],
     ['return of the jedi', 134, 1983, 'drama', 'After rescuing Han Solo from the palace of Jabba the Hutt, the rebels attempt to destroy the second Death Star, while Luke struggles to make Vader return from the dark side of the Force.'],
@@ -17,64 +17,18 @@ var movies = [
     ['serenity', 119, 2005, 'drama', 'The crew of the ship Serenity tries to evade an assassin sent to recapture one of their number who is telepathic.'],
     ['the hitchhiker\'s guide to the galaxy', 109, 2005, 'comedy', 'Mere seconds before the Earth is to be demolished by an alien construction crew, journeyman Arthur Dent is swept off the planet by his friend Ford Prefect, a researcher penning a new edition of "The Hitchhiker\'s Guide to the Galaxy."']
 ];
-var moviesObj = [];
+var movieObjs = [];
 
+/*=========================================== event listeners =======================================================*/
 
-/*============================== event space ========================================*/
-
-list.addEventListener('click', function (evt) {
-    var rel = evt.target.getAttribute('rel');
-    if(rel) {
-        alert(rel);
-    }
-});
-
+list.addEventListener('click', listClick);
 search.addEventListener('submit', find);
+newMovieBtn.addEventListener('click', showNewMovieForm);
+movieForm.addEventListener('submit', newMovieSubmit);
 
-newMovieBtn.addEventListener('click', function () {
-    newMovieForm.classList.remove('hidden');
-});
+/*====================================== constructors and prototypes ================================================*/
 
-movieForm.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-    if(!movieForm.movieTitle.value) {
-        newMovieForm.classList.add('hidden');
-        return '';
-    }
-    var genreInputs = document.getElementsByName('genre');
-    var genre;
-    list.innerHTML = '';
-
-    for(var i = 0; i < genreInputs.length; i++) {
-        var genreInput = genreInputs[i];
-        if(genreInput.checked) {
-            genre = genreInput.value;
-            break;
-        }
-    }
-
-    var movie = [movieForm.movieTitle.value, movieForm.runTime.value, movieForm.release.value, genre, movieForm.description.value];
-    debugger;
-    movies.push(movie);
-    movieForm.reset();
-    newMovieForm.classList.add('hidden');
-    capTheArray();
-    makeObjAndLi();
-});
-
-
-/*============================== function space ========================================*/
-
-//capitalizes first letter of a string
-function capitalize(str) {
-    if (!str || typeof str !== "string") {
-        return str;
-    }
-
-    return str[0].toUpperCase() + str.slice(1);
-}
-
-//function for creating movie objects
+// function for creating movie objects
 function Movie(title, runTime, release, genre, desc) {
     this.title = title;
     this.runTime = runTime;
@@ -96,7 +50,18 @@ Movie.create = function create(title, runTime, release, genre, desc) {
     return new Movie(title, runTime, release, genre, desc);
 };
 
-//creates new elements
+/*============================================= functions ===========================================================*/
+
+// capitalizes first letter of a string
+function capitalize(str) {
+    if (!str || typeof str !== "string") {
+        return str;
+    }
+
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+// creates new elements
 function e(elementType, text, atributes, styles, selector) {
     var element = document.createElement(elementType);
     element.textContent = text || '';
@@ -122,7 +87,7 @@ function e(elementType, text, atributes, styles, selector) {
     return element;
 }
 
-//searches movie array for a specified movie
+// searches movie array for a specified movie
 function find(evt) {
     evt.preventDefault();
 
@@ -145,46 +110,85 @@ function find(evt) {
     }
 }
 
-//cyles through movies array and calls capitalize for each word in the titles
+// alerts a movie's short description when it is clicked on
+function listClick(evt) {
+    console.log(evt.target.nodeName, evt.target.hasOwnProperty('rel'));
+    if(evt.target.nodeName !== 'LI' || evt.target.nodeName !== 'UL') {
+        alert(evt.target.parentNode.getAttribute('rel'));
+    } else if (evt.target.hasOwnProperty('rel')) {
+        alert(evt.target.getAttribute('rel'));
+    }
+}
+
+// sows the new movie form
+function showNewMovieForm() {
+    newMovieForm.classList.remove('hidden');
+}
+
+// when the movie form is subitted this takes the data and creates a new movie object and clears and reloads the list
+function newMovieSubmit(evt) {
+    evt.preventDefault();
+    if(!movieForm.movieTitle.value) {
+        newMovieForm.classList.add('hidden');
+        return '';
+    }
+
+    // clear movie list in the DOM
+    list.innerHTML = '';
+
+    var movie = [
+        movieForm.movieTitle.value,
+        movieForm.runTime.value,
+        movieForm.release.value,
+        movieForm.genre.value,
+        movieForm.description.value
+    ];
+    movies.push(movie);
+    movieForm.reset();
+    newMovieForm.classList.add('hidden');
+
+    // rebuilds the movie list in the DOM
+    capTheArray();
+    makeObjAndLi();
+}
+
+// cycles through movies array and calls capitalize for each word in the titles
 function capTheArray() {
     for (var i = 0; i < movies.length; i++) {
         var titleWords = movies[i][0].split(' ');
         for (var j = 0; j < titleWords.length; j++) {
-            var noCap = ['of', 'the', 'and', 'for', 'in', 'to'];
+            var noCap = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "it", "or", "nor"];
             if (j !== 0 && noCap.indexOf(titleWords[j]) > -1) {
-                continue;
+                if (!titleWords[(j-1)].match(/:/g))
+                    continue;
             }
-
             titleWords[j] = capitalize(titleWords[j]);
         }
-
         titleWords = titleWords.join(' ');
         movies[i][0] = titleWords;
     }
 }
 
-/*
- creates movie objects from the array and adds them to a
- new array and created list items for each movie object
- */
+// creates movie objects from the array and adds them to a new array and created list items for each movie object
 function makeObjAndLi() {
     for (var l = 0; l < movies.length; l++) {
         var movie = movies[l];
         var movieObj = Movie.create.apply({}, movie);
         var li = e('li', '', {'rel': movieObj.preview()}, {}, list);
-        var title = e('h3', movieObj.title, {'rel': movieObj.preview()}, {}, li);
-        var info = e('p', 'genre: ' + movieObj.genre + '  |  year released: ' + movieObj.release + '  |  running time: ' + movieObj.runningTimeHours(), {'rel': movieObj.preview()}, {}, li);
-
-        /*li.addEventListener('click', function () {
-         alert(this.getAttribute('rel'));
-         });*/
-
-        moviesObj.push(movieObj);
+        var title = e('h3', movieObj.title, {}, {}, li);
+        var info = e('p',
+          'genre: ' + movieObj.genre + '  |  year released: ' + movieObj.release + '  |  running time: ' + movieObj.runningTimeHours(),
+          {},
+          {},
+          li);
+        movieObjs.push(movieObj);
     }
+    localStorage.movies = JSON.stringify(movies);
 }
 
-
-/*============================== execution space ========================================*/
+/*========================================== execution ==============================================================*/
 
 capTheArray();
 makeObjAndLi();
+
+document.querySelector()
