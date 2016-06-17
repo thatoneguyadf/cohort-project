6e25365bcac8b5c44fd0adef8adc8593d3176b80
thatoneguyadf <adamfield@live.com>
2016-06-17 09:53:35 -0700

    began conversion to jquery

--- cohortWeekFinalProject/js/helpers.js
+++ cohortWeekFinalProject/js/helpers.js
@@ -1,20 +1,20 @@
 (function () {
   // creates new elements
   function e(elementType, text, atributes, styles, selector) {
-    var element = document.createElement(elementType);
-    element.textContent = text || '';
+    var $element = $('<' + elementType + '></' + elementType +'>');
+    $element.text(text);
     for(var attr in atributes) {
       if(atributes.hasOwnProperty(attr))
-        element.setAttribute(attr, atributes[attr]);
+        $element.attr(attr, atributes[attr]);
     }
     for(var style in styles) {
       if(styles.hasOwnProperty(style))
-        element.style[style] = styles[style];
+        $element.css(styles);
     }
-    var container = typeof(selector) === 'string' ? document.querySelector(selector) : selector;
-    if(container)
-      container.appendChild(element);
-    return element;
+    var $container = typeof(selector) === 'string' ? $(selector) : selector;
+    if($container)
+      $container.append($element);
+    return $element;
   }
   // generates a random number based on max number passed in
   function randomNum(max) {
